993021ec62de50f8bbb3f7ebd1e2a57f6c92e69b
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2018-10-20 14:54:56 -0700

    changed background color to yellow

--- index.js
+++ index.js
@@ -10,14 +10,30 @@ const fs = require('fs');
 function Bitmap(filePath) {
   this.file = filePath;
 }
-
 /**
  * Parser -- accepts a buffer and will parse through it, according to the specification, creating object properties for each segment of the file
  * @param buffer
  */
 Bitmap.prototype.parse = function(buffer) {
-  this.type = buffer.toString('utf-8', 0, 2);
+    // this.type = buffer.toString('utf-8', 0, 2);
   //... and so on
+  //   this.buffer = Buffer.from(buffer);
+    this.type = buffer.toString('utf-8', 0, 2);
+    this.fileSize = buffer.readInt32LE(2); // skips 2 read 32 bytes
+    this.bytesPerPixel = buffer.readInt16LE(28);
+    this.height = buffer.readInt32LE(22);
+    this.width = buffer.readInt32LE(18);
+    this.buffer = Buffer.from(buffer, this.width, this.height);
+    // testing out Ed's idea
+    this.pixelArrayOffset = buffer.readInt32LE(10);
+    this.rawPixels = buffer.slice(this.pixelArrayOffset);
+    this.headers = buffer.slice(0, this.pixelArrayOffset);
+    // console.log('width', this.width);
+    // console.log(buffer.readInt32LE(10));
+    // console.log(JSON.stringify(buffer.slice(buffer.readInt32LE(10))));
+
+
+
 };
 
 /**
@@ -27,7 +43,7 @@ Bitmap.prototype.parse = function(buffer) {
 Bitmap.prototype.transform = function(operation) {
   // This is really assumptive and unsafe
   transforms[operation](this);
-  this.newFile = this.file.replace(/\.bmp/, `.${operation}.bmp`);
+  // this.newFile = this.file.replace(/\.bmp/, `.${operation}.bmp`);
 };
 
 /**
@@ -36,13 +52,35 @@ Bitmap.prototype.transform = function(operation) {
  * Pro Tip: Use "pass by reference" to alter the bitmap's buffer in place so you don't have to pass it around ...
  * @param bmp
  */
+const transformBackground = (bmp) => {
+
+    bitmap.newFile = bitmap.file.replace(/\.bmp/, `.${operation}.bmp`);
+    console.log('Transforming bitmap into greyscale', bmp);
+
+    let changeImg = bmp.buffer;
+
+    for(let i = 0; i < changeImg.length ; i= i+3)  {
+        if (changeImg[i] > 220 && changeImg[i+1] < 170 && changeImg[i+2] >220){
+            changeImg[i] = 200;
+            changeImg[i+1] = 50;
+            changeImg[i+2] = 200;
+        };
+    }
+
+    fs.writeFile(bitmap.newFile, bmp.buffer, (err,out) =>{
+       if (err) {
+           throw err;
+       }
+        console.log(`Bitmap Transformed: ${bitmap.newFile}`);
+    });
+};
 const transformGreyscale = (bmp) => {
 
-  console.log('Transforming bitmap into greyscale', bmp);
+    console.log('Transforming bitmap into greyscale', bmp);
 
-  //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
+    //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
 
-  //TODO: alter bmp to make the image greyscale ...
+    //TODO: alter bmp to make the image greyscale ...
 
 };
 
@@ -51,7 +89,8 @@ const transformGreyscale = (bmp) => {
  * Each property represents a transformation that someone could enter on the command line and then a function that would be called on the bitmap to do this job
  */
 const transforms = {
-  greyscale: transformGreyscale
+    greyscale: transformGreyscale,
+    background: transformBackground
 };
 
 // ------------------ GET TO WORK ------------------- //
@@ -70,12 +109,12 @@ function transformWithCallbacks() {
 
     // Note that this has to be nested!
     // Also, it uses the bitmap's instance properties for the name and thew new buffer
-    fs.writeFile(bitmap.newFile, bitmap.buffer, (err, out) => {
-      if (err) {
-        throw err;
-      }
-      console.log(`Bitmap Transformed: ${bitmap.newFile}`);
-    });
+    // fs.writeFile(bitmap.newFile, bitmap.buffer, (err, out) => {
+    //   if (err) {
+    //     throw err;
+    //   }
+    //   console.log(`Bitmap Transformed: ${bitmap.newFile}`);
+    // });
 
   });
 }
